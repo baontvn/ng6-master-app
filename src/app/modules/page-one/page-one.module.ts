32aeeeff166be0706a8d50bef817f2e511b7a803
Nguyen Bao <nguyenbao@Nguyens-MacBook-Pro-2.local>
2018-09-11 14:04:03 +0700

    Remove material module and add full-calendar sample

--- src/app/modules/page-one/page-one.module.ts
+++ src/app/modules/page-one/page-one.module.ts
@@ -4,13 +4,15 @@ import {PageOneRoutingModule} from './page-one-routing.module';
 import {SharedModule} from '../../shared/shared.module';
 import { PageOneMainComponent } from './pages/page-one-main/page-one-main.component';
 import { PageOneDetailComponent } from './pages/page-one-detail/page-one-detail.component';
+import { FullCalendarModule } from 'ng-fullcalendar';
 
 @NgModule({
   imports: [
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
-    PageOneRoutingModule
+    PageOneRoutingModule,
+    FullCalendarModule
   ],
   declarations: [
     PageOneMainComponent,
--- src/app/modules/page-one/pages/page-one-main/page-one-main.component.ts
+++ src/app/modules/page-one/pages/page-one-main/page-one-main.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NzModalService } from 'ng-zorro-antd';
-
+import { CalendarComponent } from 'ng-fullcalendar';
+import { Options } from 'fullcalendar';
 @Component({
   selector: 'app-page-one-main',
   templateUrl: './page-one-main.component.html',
   styleUrls: ['./page-one-main.component.scss']
 })
 export class PageOneMainComponent implements OnInit {
+  displayEvent: any;
+  calendarOptions: Options;
+  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
 
   constructor(private modalService: NzModalService) { }
 
   ngOnInit() {
+    this.calendarOptions = {
+      editable: true,
+      eventLimit: false,
+      businessHours: true,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listMonth'
+      },
+      events: [
+        {
+          id: '1',
+          title: 'Birthday Party',
+          start: new Date(),
+          end: new Date(),
+          className: 'task-priority-high',
+          // color: 'red',
+          // textColor: 'white'
+        },
+        {
+          id: 2,
+          title: 'Pool Party',
+          start: new Date(),
+          className: 'task-priority-low',
+          color: 'blue',
+          textColor: 'white'
+        }
+      ]
+    };
   }
 
   showConfirm(): void {
@@ -19,6 +52,42 @@ export class PageOneMainComponent implements OnInit {
       nzContent: '<b>Some descriptions</b>',
       nzOnOk   : () => console.log('OK')
     });
+  }
+
+
+  clickButton(model: any) {
+    this.displayEvent = model;
+  }
+
+  eventClick(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title,
+        allDay: model.event.allDay,
+        hehe: model.event.tata
+        // other params
+      },
+      duration: {}
+    };
+    this.displayEvent = model;
+  }
+  updateEvent(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title
+        // other params
+      },
+      duration: {
+        _data: model.duration._data
+      }
+    };
+    this.displayEvent = model;
   }
 
 }
